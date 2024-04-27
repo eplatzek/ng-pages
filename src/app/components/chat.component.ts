5f3df43a90a7163303d3805557f5a2a8800d57f3
ericplatzek <ericplatzek@gmail.com>
2024-04-27 11:02:34 -0700

    tweak the textarea styling

--- src/app/components/chat.component.ts
+++ src/app/components/chat.component.ts
@@ -6,12 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   standalone: true,
   selector: 'app-chat',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule],
   templateUrl: './chat.component.html'
 })
 
